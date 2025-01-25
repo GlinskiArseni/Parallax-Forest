5032cd853b2ed3fa4e8f1bef74d67da9bab5206e
arsenij <arsenij.glinski123@gmail.com>
2025-01-25 16:31:53 +0300

    end.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 window.addEventListener('scroll', e=> {
-document.body.style.cssText = `--scrollTop: ${this.scrollY}px`
+document.body.style.cssText += `--scrollTop: ${this.scrollY}px`
+})
+gsap.registerPlugin(ScroollTrigger, ScroollSmoother)
+ScroollSmoother.create({
+wrapper: '.wrapper',
+content: '.content'
 })
